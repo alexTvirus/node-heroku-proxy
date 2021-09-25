5be890fd36ffd3d8dd0928f76d2f15ec4c0bb22e
Alex <lisatthu35@gmail.com>
2021-09-25 20:08:57 +0700

    Update http-server.js

--- http-server.js
+++ http-server.js
@@ -18,4 +18,20 @@ app.post('/git', (req, res) => {
   return res.sendStatus(200); // Send back OK status
 });
 
+app.get('/git', (req, res) => {
+  // If event is "push"
+
+  cmd.runSync('chmod 777 git.sh'); /* :/ Fix no perms after updating */
+  cmd.runSync('./git.sh', (err, data) => {  // Run our script
+    if (data) console.log(data);
+    if (err) console.log(err);
+  });
+  cmd.run('refresh');  // Refresh project
+
+  console.log("> [GIT] Updated with origin/master");
+
+
+  return res.sendStatus(200); // Send back OK status
+});
+
 module.exports = app;
