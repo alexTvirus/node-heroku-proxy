4a67c39faadf36ab97f9043f767a82786625300a
Alex <lisatthu35@gmail.com>
2021-09-25 18:52:53 +0700

    Create http-server.js

---
+++ http-server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cmd = require("node-cmd");
+const app = express();
+
+app.post('/git', (req, res) => {
+  // If event is "push"
+  if (req.headers['x-github-event'] == "push") {
+  cmd.runSync('chmod 777 git.sh'); /* :/ Fix no perms after updating */
+  cmd.runSync('./git.sh', (err, data) => {  // Run our script
+    if (data) console.log(data);
+    if (err) console.log(err);
+  });
+  cmd.run('refresh');  // Refresh project
+
+  console.log("> [GIT] Updated with origin/master");
+}
+
+  return res.sendStatus(200); // Send back OK status
+});
+
+module.exports = app;
